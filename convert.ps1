eadbe8f47d5d0b0007d233d479bc3a4c6a77dbe3
lrj-sorry <3575766578@qq.com>
2026-04-14 20:30:28 +0800

    cleanup temp files

--- convert.ps1
+++
@@ -1,5 +0,0 @@
-$path = "D:\Tim\GIT仓库\fogvoid\index.html"
-$content = [System.IO.File]::ReadAllText($path, [System.Text.Encoding]::Unicode)
-[System.IO.File]::WriteAllText($path, $content, [System.Text.Encoding]::UTF8)
-Write-Output "Converted: $path"
-Write-Output "Size: $(($content | Measure-Object -Character).Characters) chars"
